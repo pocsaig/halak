671a06de0556f06422be263b1afc0628eb0eeecc
pocsaig <112682762+pocsaig@users.noreply.github.com>
2025-02-17 11:18:07 +0100

    Delete halak_backend directory

--- halak_backend/halak_backend/DTOk/HorgaszottHalDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace halak_backend.DTOk
-{
-    public class HorgaszottHalDTO
-    {
-        public string HorgaszNev { get; set; }
-        public string HalNev { get; set; }
-        public DateTime Datum { get; set; }
-    }
-}
