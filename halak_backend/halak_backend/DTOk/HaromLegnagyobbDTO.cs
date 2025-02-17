9682f3eff8a1eb1debee03debfceb0bed5e4ca28
pocsaig <112682762+pocsaig@users.noreply.github.com>
2025-02-17 11:17:34 +0100

    Delete halak_backend directory

--- halak_backend/halak_backend/DTOk/HaromLegnagyobbDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace halak_backend.DTOk
-{
-    public class HaromLegnagyobbDTO
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public decimal MeretCm { get; set; }
-    }
-}
